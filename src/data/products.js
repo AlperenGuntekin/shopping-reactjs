bbf33e8b7b506f4a2049817dbfaabcd6638c0dba
AlperenGuntekin <alprngntkn@hotmail.com>
2022-07-02 15:06:00 +0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Propolis Tinktur',
+    price: 499.99,
+    currency: 'TL',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Immunkraft',
+    price: 249.99,
+    currency: 'TL',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Propolis Halsspray',
+    price: 239.99,
+    currency: 'TL',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Propolis Honigsirup',
+    price: 119.99,
+    currency: 'TL',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Gelée Royale',
+    price: 599.99,
+    currency: 'TL',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Gelée Blütenpollen',
+    price: 149.99,
+    currency: 'TL',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
